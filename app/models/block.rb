298a176fb53db536ca089d7503d1d60e33698df5
TheMisterPanek <mrPanek@yandex.by>
2017-07-17 22:28:22 +0300

    add anotate to block

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: blocks
+#
+#  id         :integer          not null, primary key
+#  page_id    :integer
+#  data       :text
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Block < ApplicationRecord
   belongs_to :page
 end
